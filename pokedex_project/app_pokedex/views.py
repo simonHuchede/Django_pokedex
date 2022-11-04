7dd1aa6ec0a373274d9ece96979c2635d98606b5
Michrax <michael.lapeyrere@gmail.com>
2022-11-04 14:32:49 +0100

    first commit

--- pokedex_project/app_pokedex/views.py
+++ pokedex_project/app_pokedex/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+def index(request):
+    Text="<h1>Pokédex en construction</h1>"
+    return HttpResponse(Text)
+
