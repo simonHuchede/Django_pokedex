efb57c0fe68187c08f390ce3780bf64357488ffe
Curzeed <curze@gmail.com>
2022-11-06 00:41:13 +0100

    dcsc

--- pokedex_project/app_pokedex/views.py
+++ pokedex_project/app_pokedex/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from random import randrange
 import requests as req
 
 def index(request):
-    url="https://pokeapi.co/api/v2/pokemon/?limit=1155"
+    rdm = randrange(100)
+    url="https://pokeapi.co/api/v2/pokemon/"+str(rdm)
     response = req.get(url)
+    
     if response.status_code == 200:
         results = response.json()
-        pokemonList = results['results']
+        pokemon = results
+        stats = {
+            "hp" : pokemon["stats"][0],
+            "atq" : pokemon["stats"][1],
+            "def" : pokemon["stats"][2],
+            "atqSpe" : pokemon["stats"][3],
+            "defSpe" : pokemon["stats"][4],
+            "speed" : pokemon["stats"][5]
+        }
         context={
-    'pokemonList': pokemonList
-    }
+            'pokemon': pokemon,
+            'url' : pokemon['sprites']['other']['official-artwork']['front_default'],
+            "stats" : stats
+        }
     return render(request,"app_pokedex/index.html",context)
 
