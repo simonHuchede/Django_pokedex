9d1b35b2ef22128a2d3bb1e31f49dd007b45e838
simonhuchede <simonhuchede@gmail.com>
2022-11-05 20:25:14 +0100

    -feat: add pokemon object to views

--- pokedex_project/app_pokedex/views.py
+++ pokedex_project/app_pokedex/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests as req
+from .models import Pokemon
 
 def index(request):
-    url="https://pokeapi.co/api/v2/pokemon/?limit=1155"
+    url="https://pokeapi.co/api/v2/pokemon/?limit=151"
     response = req.get(url)
     if response.status_code == 200:
         results = response.json()
@@ -11,5 +12,13 @@ def index(request):
         context={
     'pokemonList': pokemonList
     }
-    return render(request,"index.html",context)
+        pokemonObjList=[]
+        for pokemon in pokemonList:
+            res = req.get(pokemon['url'])
+            pokemonjs = res.json()
+            pokemonobj = Pokemon(pokemonjs['id'],pokemonjs['name'],pokemonjs['weight'],pokemonjs['height'],pokemonjs['types'][0]['type']['name'],pokemonjs['base_experience'],pokemonjs['abilities'][0]['ability']['name'],pokemonjs['sprites']['other']['dream_world']['front_default'])
+            pokemonObjList.append(pokemonobj)
+
+        print(pokemonObjList[0].weight)
+    return render(request,"app_pokedex/index.html",context)
 
