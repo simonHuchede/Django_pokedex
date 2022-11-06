79aa608b4f0349831b818dd363d4cf725ec1e460
Curzeed <curze@gmail.com>
2022-11-06 02:01:58 +0100

    Add chart.js for the stats ux

--- pokedex_project/app_pokedex/static/js/index.js
+++ pokedex_project/app_pokedex/static/js/index.js
@@ -1,27 +1,38 @@
-
+const stats = {
+    'hp' : document.getElementById('hp').value,
+    'atq' : document.getElementById('atq').value,
+    'def' : document.getElementById('def').value,
+    'atqSpe' : document.getElementById('atqspe').value,
+    'defSpe' : document.getElementById('defspe').value,
+    'speed' : document.getElementById('speed').value
+}
+console.log(stats)
 const labels = [
-    'Force' ,
-    'Intelligence',
-    'Sagesse',
-    'Puissance'
+    'HP' ,
+    'Attack',
+    'Defense',
+    'Attack special',
+    'Defense special',
+    'speed'
 ]
 const data = {
     labels : labels,
     datasets : [{
-        label : 'My first dataset',
+        label : 'Statistics',
         backgroundColor : [
             'rgb(255, 99, 132)',
             'rgb(54, 162, 235)',
             'rgb(255, 205, 86)',
             'rgb(55, 429, 501)',
             'rgb(24, 62, 25)',
+            'rgb(55, 429, 501)',
         ],
         borderColor : 'rgb(255,99,132)',
-        data : [300,50,40,64]
+        data : [stats.hp,stats.atq,stats.def,stats.atqSpe,stats.defSpe,stats.speed]
     }]
 }
 const config = {
-    type : 'pie',
+    type : 'bar',
     data : data,
     options : {
         responsive : false,
