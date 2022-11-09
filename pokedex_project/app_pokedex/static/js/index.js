3482aa132112f7319b4a9b5281c5af8797c83fd1
Curzeed <xavier.lecapitaine4450@gmail.com>
2022-11-09 14:53:17 +0100

    - add dark/light theme - add animation on nav item hover

--- pokedex_project/app_pokedex/static/js/index.js
+++ pokedex_project/app_pokedex/static/js/index.js
@@ -19,6 +19,7 @@ const data = {
     labels : labels,
     datasets : [{
         label : 'Statistics',
+        labels : labels,
         backgroundColor : [
             'rgb(255, 99, 132)',
             'rgb(54, 162, 235)',
@@ -41,7 +42,32 @@ const config = {
     type : 'bar',
     data : data,
     options : {
-        responsive : false,
+        interaction : {
+            mode : 'index'
+        },
+        responsive : true,
+        maintainAspectRatio : false,
     }
 }
 const chart = new Chart(document.getElementById('myChart'),config);
+const triggerDmode = document.getElementById('trigger')
+
+// darkMode gestion
+triggerDmode.addEventListener('click', darkMode)
+function darkMode(){
+    let body = document.body
+    let iconDark = document.querySelector('#dark_icon')
+    let iconLight = document.querySelector('#light_icon')
+    if(body.classList.contains('light-theme')){
+        body.classList.add('dark-theme')
+        body.classList.remove('light-theme')
+        iconDark.style = "display : block";
+        iconLight.style= "display : none";
+    }else{
+        body.classList.add('light-theme')
+        body.classList.remove('dark-theme')
+        iconDark.style = "display : none";
+        iconLight.style= "display : block";
+    }
+        
+}
