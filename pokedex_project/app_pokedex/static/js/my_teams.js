fbeb87b3702033fe03871ab34de0b1dd36527829
Curzeed <xavier.lecapitaine4450@gmail.com>
2022-11-14 16:11:31 +0100

    My team :
    - add css 
    - stylize pokemon-card
    - add some javascript events
    - Every functions in js are documented

--- pokedex_project/app_pokedex/static/js/my_teams.js
+++ pokedex_project/app_pokedex/static/js/my_teams.js
@@ -1,71 +1,78 @@
-const dropdown = document.querySelector('.dropdown')
-dropdown.addEventListener('click', () => {
-    dropdown.classList.toggle('active')
-})
-function setText(value) {
-    document.querySelector('.textBox').value = value
-}
-const input = document.querySelector('.textBox')
-
-input.addEventListener('keyup', (e)=> {
-    const value = input.value.toUpperCase();
-    const opt = document.querySelectorAll('.item')
-    for( i = 0; i < opt.length; i++){
-        let txtValue = opt[i].textContent || opt[i].innerText;
-        if (txtValue.toUpperCase().indexOf(value)> -1){
-            opt[i].style.display = "";
-        }else{
-            opt[i].style.display = "none"
-        }
-    }
-})
-document.querySelectorAll('.item').forEach((item) => {
-    item.addEventListener('click',() => {
-        if(document.querySelector('.buildTeam').childElementCount >= 3){
-            let flash = document.querySelector('.flash');
-            flash.classList.add('flash-is-showing');
-            flash.textContent = "You only can have 3 pokemons in your team !"
-            return;
-        }else{
-            fetch(`https://pokeapi.co/api/v2/pokemon/${item.textContent.toLowerCase()}`)
-       .then(res => res.json())
-       .then(
-        res => {
-            let template = document.getElementById('template_card')
-            let clone = template.content.cloneNode(true)
-            let card_img = clone.querySelector('.card_pokemon_team_img')
-            let div = clone.querySelector('.card_pokemon_team')
-            let img = document.createElement('img')
-            let svg = clone.querySelector('svg')
-            //let typeDiv = clone.querySelector('.card-type')
-            //let urlType = clone.querySelector('.url')
-            //if(res.types.length >= 1){
-            //    for(i=0; i< res.types.length; i++){
-            //        let type = res.types[i].type.name
-            //        let img = document.createElement('img')
-            //        let span = document.createElement('span')
-            //       img.src = `${urlType.value+type}.svg`
-            //        img.classList.add('svg-type')
-            //        img.alt = "type_pokemon"
-            //       span.textContent = type
-            //        typeDiv.appendChild(img)
-            //       typeDiv.appendChild(span)
-            //    }
-            //}
-            svg.value = item.textContent
-            let spanTitle = clone.querySelector('#pokemon_name')
-            div.classList.add(item.textContent)
-            img.src = res.sprites.other.dream_world.front_default
-            card_img.append(img)
-            spanTitle.textContent = item.textContent
-            document.querySelector('.buildTeam').appendChild(clone)
-            })
-        }
-    })
-})
+/**
+* remove the pokemon via this
+*/
 function deletePokemon(any){
     document.querySelector('.'+any.value).remove()
 }
+
+/*
+* add a flash message 
+*/
 document.querySelector('.flash').addEventListener('animationend', function(){
     document.querySelector('.flash').classList.remove('flash-is-showing');
-  })
+})
+
+/*
+* function that return a builded canvas  from chartJs 
+* provide an array of raw stats 
+*/
+function getStats(statsRaw, div) {
+    const stats = {
+        'hp' : statsRaw[0].base_stat,
+        'atq' : statsRaw[1].base_stat,
+        'def' : statsRaw[2].base_stat,
+        'atqSpe' : statsRaw[3].base_stat,
+        'defSpe' : statsRaw[4].base_stat,
+        'speed' : statsRaw[5].base_stat
+    }
+    const labels = [
+    'HP',
+    'Attack',
+    'Defense',
+    'Attack special',
+    'Defense special',
+    'Speed']
+    const data = {
+    labels : labels,
+    datasets : [{
+        backgroundColor : [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+            'rgb(55, 429, 501)',
+            'rgb(24, 62, 25)',
+            'rgb(55, 429, 501)',
+        ],
+        borderColor : 'rgb(255,99,132)',
+        data : [stats.hp,
+            stats.atq,
+            stats.def,
+            stats.atqSpe,
+            stats.defSpe,
+            stats.speed,
+        ],
+        maxBarThickness: 15
+    }
+    ]
+    }
+    const config = {
+    type : 'bar',
+    data : data,
+    options : {
+        responsive : true,
+        labels : false,
+        plugins : {
+            legend : {
+                display : false,
+                },
+            
+        },
+        scales : {
+            y : {
+                beginAtZero: true
+            }
+        }
+    }
+    }
+    const chart = new Chart(div,config);
+}
